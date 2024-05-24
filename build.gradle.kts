adf6e3b8cd4ec47b6dbd3aefff8d0c94f77a1a6a
max-sliva <persimon@inbox.ru>
2024-05-24 16:41:52 +0500

    updated gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,25 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.github.java-native:jssc:2.9.6")
+    implementation("org.json:json:20231013")
+    implementation("com.formdev:flatlaf:3.4.1") //look'n'feel
+
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 kotlin {
-    jvmToolchain(19)
+    jvmToolchain(17)
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "SimpleExKt"
+    }
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
 }
